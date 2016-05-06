3d5f30c63406bb687b91d9acc6b3c51d70d472cb
Kerz <kerzo@mail.ru>
2016-05-06 08:15:19 +0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
+
+  validates_presence_of     :name
+  validates_presence_of     :password, allow_blank: true
+  validates_presence_of     :password_confirmation
+  validates_confirmation_of :password
 end
