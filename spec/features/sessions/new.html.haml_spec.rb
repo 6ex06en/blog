9f23a5b33c94e5112011892dd5242923fceb8bd7
Kerz <kerzo@mail.ru>
2016-05-08 08:41:51 +0300

    authorization specs

---
+++ spec/features/sessions/new.html.haml_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Авторизация", type: "feature", js: true do
+  let!(:user) {User.create(name: "test", password: "test",
+                                        password_confirmation: "test")}
+
+  describe "когда все поля заполнены верно" do
+
+    before {
+      visit signin_path
+      fill_in           "Имя",    with: user.name
+      fill_in           "Пароль", with: user.password
+      click_button      "Войти"
+    }
+
+    it "должна отобразиться имя авторизованного пользователя" do
+      expect(page).to have_content(user.name)
+    end
+
+    it "должна отрендериться главная страница" do
+      expect(page).to have_selector(".pagination")
+    end
+  end
+
+  describe "когда поля c ошибками" do
+
+    before {
+      visit signin_path
+      fill_in           "Имя",    with: user.name
+      fill_in           "Пароль", with: "error"
+      click_button      "Войти"
+    }
+
+    it "должно отобразиться flash сообщение" do
+      expect(page).to have_content("Неверный пользователь или пароль")
+    end
+
+    it "не должно отражаться имя авторизованного пользователя" do
+      expect(page).not_to have_content(user.name)
+    end
+
+  end
+end
