558f243ca9455636dce2e1744859e1e14d26c03f
Kerz <kerzo@mail.ru>
2016-05-09 08:37:22 +0300

    delete picture from cloud after destroy post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
 class Post < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
-  
   belongs_to :user
-  
+
   validates_presence_of :name, :content, :user_id, :picture
-  
+
+  before_destroy :delete_image_from_cloud
+
+  private
+
+  def delete_image_from_cloud
+    Thread.new { a.remove_picture! }   
+  end
+
 end
